7c2098f54a2dbd77153716cfc2acabf009438e9b
pymitch <mitchell.berggren@gmail.com>
2017-06-07 16:49:24 -0500

    second commit, added app.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wod
+This will be an app to learn Merriam Webster's words of the day
--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const path = require('path');
 
-app = express();
+// initalize app
+const app = express();
+
+// set home route
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+//start server
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
+});
