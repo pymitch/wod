c07bdec862f4992e77a6cbc6e0c922c672a4e17f
pymitch <mitchell.berggren@gmail.com>
2017-06-16 11:48:01 -0500

    UI improvement

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
-    // let word = ""
-    // let defs = []
-    // let position = 0
-    //$.getJSON("./public/js/newWord.json", function(data){
-        console.log($(document).data)
-        word = document.data.word
-        defs = data.definitions
-        position = data.position
-        $(".1").html(word)
-        $("#0").html(defs[0]);
-        $("#1").html(defs[1]);
-        $("#2").html(defs[2]);
-        $("#3").html(defs[3]);
-        $("#1").html("hey")
-    //});
-});
+    $('.next-button').fadeOut()
+    // alert(position)
+    $('.box').hover(
+        function(){
+        $(this).addClass('hover')
+        }, function (){
+        $(this).removeClass('hover')
+        }
+    )
+    $('.box').click(
+        function(){
+            // console.log(typeof(this.id))
+            // console.log(typeof(position))
+            // console.log((this.id == position))
+            if(this.id == position){
+                $(this).addClass('right')
+            } else {
+                $(this).addClass('wrong')
+            }
+            $('.next-button').fadeIn()
+        }
+    )
+})
