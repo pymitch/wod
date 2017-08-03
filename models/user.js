68025998e02673fdcbc59c2a87a37a733a48ea6e
pymitch <mitchell.berggren@gmail.com>
2017-08-03 16:07:25 -0500

    added register page

--- models/user.js
+++ models/user.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose')
 var passportLocalMongoose = require('passport-local-mongoose')
 var userSchema = new mongoose.Schema({
-    user: {
+    username: {
         type: String, 
         required: true
     },
-    bins:{
-        bin1:[],
-        bin2:[],
-        bin3:[],
-        bin4:[],
-        bin5:[]
-    },  
-    lastLogin: Date,
     password: {
         type: String,
         required: true
@@ -22,3 +14,12 @@ var userSchema = new mongoose.Schema({
 userSchema.plugin(passportLocalMongoose)
 
 const User = module.exports = mongoose.model('User', userSchema)
+
+    // bins:{
+    //     bin1:[],
+    //     bin2:[],
+    //     bin3:[],
+    //     bin4:[],
+    //     bin5:[]
+    // },  
+    // lastLogin: Date,
